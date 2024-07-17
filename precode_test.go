a5315f8d9c93a7954783206ee5283f1c1864c473
Бугай Дмитрий Олегович <y1nkee@bk.ru>
2024-07-17 15:59:25 +0300

    first

---
+++ precode_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMainHandlerValidRequest(t *testing.T) {
+	req, err := http.NewRequest("GET", "/cafe?city=moscow&count=2", nil)
+	require.NoError(t, err)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	require.Equal(t, http.StatusOK, responseRecorder.Code)
+	assert.NotEmpty(t, responseRecorder.Body.String())
+}
+
+func TestMainHandlerInvalidCity(t *testing.T) {
+	req, err := http.NewRequest("GET", "/cafe?city=invalid&count=2", nil)
+	require.NoError(t, err)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	require.Equal(t, http.StatusBadRequest, responseRecorder.Code)
+	assert.Equal(t, "wrong city value", responseRecorder.Body.String())
+}
+
+func TestMainHandlerWhenTotal(t *testing.T) {
+	totalCount := 4
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	require.Equal(t, http.StatusOK, responseRecorder.Code)
+
+	list := strings.Split(responseRecorder.Body.String(), ",")
+	assert.Len(t, list, totalCount)
+}
